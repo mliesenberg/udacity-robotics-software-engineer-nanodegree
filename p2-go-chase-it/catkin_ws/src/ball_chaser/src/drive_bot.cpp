5b362c7ad057d280874f4b656abfe36e625874a4
Martin Liesenberg <martin.liesenberg@bcgdv.com>
2019-11-16 14:17:22 +0100

    use values from request

--- p2-go-chase-it/catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ p2-go-chase-it/catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -5,19 +5,21 @@
 // ROS::Publisher motor commands;
 ros::Publisher motor_command_publisher;
 
-// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+
 // This function should publish the requested linear x and angular velocities to the robot wheel joints
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
 
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res) {
 	
-	ROS_INFO("DriveToTarget request received");
+	ROS_INFO("DriveToTargetRequest received - linear_x: %1.2f, angular_z: %1.2f", req.linear_x, req.angular_z);
 
     // Create a motor_command object of type geometry_msgs::Twist
     geometry_msgs::Twist motor_command;
-    // Set wheel velocities, forward [0.5, 0.0]
-    motor_command.linear.x = 0.5;
-    motor_command.angular.z = 0.0;
+
+    // Set wheel velocities
+    motor_command.linear.x = req.linear_x;
+    motor_command.angular.z = req.angular_z;
+
     // Publish angles to drive the robot
     motor_command_publisher.publish(motor_command);
 
